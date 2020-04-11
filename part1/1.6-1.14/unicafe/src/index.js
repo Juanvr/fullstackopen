e63b40a7fdd4ce1e7a95056d9abc96fb5ca76dfb
Juan <jvelazquezromera@gmail.com>
2020-04-11 10:49:54 +0200

    1.12 finished

--- part1/1.6-1.14/unicafe/src/index.js
+++ part1/1.6-1.14/unicafe/src/index.js
@@ -49,12 +49,28 @@ const Statistics = ({good, neutral, bad}) =>
   }
 }
 
+const Anecdote = ({anecdote}) => <div><span>{anecdote}</span></div>;
+
 
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
+  const randomNumber = () => Math.round(Math.random() * anecdotes.length);
+  
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const [selected, setSelected] = useState(randomNumber);
+
+  const nextSelected = () => setSelected((selected + 1) % anecdotes.length); 
 
   const addGood = () => setGood(good + 1);
   const addNeutral = () => setNeutral(neutral + 1);
@@ -69,7 +85,18 @@ const App = () => {
 
       <Header text="statistics"/>
       <Statistics good={good} neutral={neutral} bad={bad} />
-      </div>
+
+      <br/>
+      <br/>
+      <br/>
+      <Anecdote anecdote={anecdotes[selected]}/>
+      <br/>
+
+      <Button name="next anecdote" handleClick={nextSelected}/>
+
+
+
+    </div>
   )
 }
 
